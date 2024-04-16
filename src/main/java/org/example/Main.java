957feed2f9dcb1cc0613cf5d75f68fb971a57478
eugenevas <eugene.vas@yandex.ru>
2024-04-16 21:41:24 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver.exe");
+    }
+}
